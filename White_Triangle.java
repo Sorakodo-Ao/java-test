de4b2199fb9b7335ac48cc68422682665f4912ae
Sorakodo-Ao <2818688939@qq.com>
2023-04-01 23:32:38 +0800

    打印空心三角形

---
+++ White_Triangle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class White_Triangle{
+	public static void main(String [] args){
+	//   *1
+	//  ***3
+	// *****5
+	//*******7
+	int i , j ;
+	Scanner input = new Scanner(System.in);
+	System.out.print("please input plies:");
+	int plies =input.nextInt();//输入层数
+	for(i = 0; i < plies; i++){
+		for(int k = 0 ; k < plies  - i ; k++){
+			System.out.print(" ");//打印*前面的空格
+		}
+		for (j = 0 ; j < 2*i + 1; j++){
+			
+			if( j == 0 || j == 2*i || i ==plies - 1){
+				System.out.print("*");//外框
+				}
+			else {System.out.print(" ");}//内部空心
+		}
+		System.out.println();//打印完一行换行
+	}
+	
+	}
+}
